4f04b4287aad50341d470176a3aa9521cd62ea32
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2023-06-09 16:46:18 -0300

    add chirps low

--- datamodule/chirps_cmip6.py
+++ datamodule/chirps_cmip6.py
@@ -2,6 +2,69 @@ import lightning.pytorch as pl
 import torch
 import numpy as np
 from torchvision import transforms
+from copy import deepcopy
+
+class Chirps(torch.utils.data.Dataset):
+    """Chirps dataset.
+    """
+
+    def __init__(self, data_dir="dataset/high-low", type='train') -> None:
+        super().__init__()
+        self.data_dir = data_dir
+        self.type = type
+        
+        if type == 'train':
+            self.chirps = np.load(f'{data_dir}/train_chirps.npy')
+        elif type == 'val':
+            self.chirps = np.load(f'{data_dir}/val_chirps.npy')
+        elif type == 'test':
+            self.chirps = np.load(f'{data_dir}/test_chirps.npy')
+        else:
+            raise ValueError(f'Invalid type: {type} (must be train, val or test)')
+        
+
+        self.image_size = self.chirps.shape[1]
+
+        # Transform nan to zero
+        self.chirps[np.isnan(self.chirps)] = 0
+        
+        # Remove images that are all zero
+        self.chirps = np.array([image for image in self.chirps if image.sum() != 0])
+
+        # find mean and std
+        self.chirps_mean = self.chirps.mean()
+        self.chirps_std = self.chirps.std()
+
+        self.transforms_chirps = transforms.Compose([
+            # transforms.ToTensor(),
+            # transforms.Pad(24),
+            # transforms.RandomCrop(self.image_size),
+            # transforms.RandomHorizontalFlip(),
+            transforms.Normalize(mean=[self.chirps_mean], std=[self.chirps_std])
+        ])
+
+    def __len__(self):
+        """Length of the dataset.
+        """
+        return len(self.chirps)
+    
+    def __getitem__(self, index):
+        """Get item.
+        """
+        chirps = self.chirps[index]
+        chirps = torch.tensor(chirps).unsqueeze(0)
+        
+        chirps = self.transforms_chirps(chirps)
+
+        chirps_low = transforms.Resize((32, 32))(chirps)
+        chirps_low = transforms.Resize((160, 160))(chirps_low)
+
+
+        # chirps_low = transforms.functional.resize(deepcopy(chirps), (32, 32))[None]
+        # chirps_low = torch.nn.functional.interpolate(chirps_low, scale_factor=5, mode='bilinear')
+
+        return chirps_low.to(torch.float32), chirps.to(torch.float32)
+
 
 class ChirpsCmip6(torch.utils.data.Dataset):
     """ChirpsCmip6 dataset.
@@ -69,6 +132,62 @@ class ChirpsCmip6(torch.utils.data.Dataset):
 
         return cmip6.to(torch.float32), chirps.to(torch.float32)
 
+
+class ChirpsDataModule(pl.LightningDataModule):
+    
+        def __init__(self, data_dir="dataset/high-low", batch_size=32) -> None:
+    
+            super().__init__()
+            
+            self.data_dir = data_dir
+            self.batch_size = batch_size
+    
+        def prepare_data(self):
+            """Prepare data.
+            """
+            ...
+    
+        def setup(self, stage=None):
+            """Setup data.
+    
+            Args:
+                stage (str, optional): Stage. Defaults to None.
+            """
+            if stage == 'fit':
+                self.train_dataset = Chirps(data_dir=self.data_dir, type='train')
+                self.val_dataset = Chirps(data_dir=self.data_dir, type='val')
+            if stage == 'test':
+                self.test_dataset = Chirps(data_dir=self.data_dir, type='test')
+    
+        def train_dataloader(self):
+            """Train dataloader.
+            """
+            return torch.utils.data.DataLoader(
+                self.train_dataset,
+                batch_size=self.batch_size,
+                shuffle=True,
+                num_workers=8
+            )
+    
+        def val_dataloader(self):
+            """Validation dataloader.
+            """
+            return torch.utils.data.DataLoader(
+                self.val_dataset,
+                batch_size=self.batch_size,
+                shuffle=False,
+                num_workers=8
+            )
+    
+        def test_dataloader(self):
+            """Test dataloader.
+            """
+            return torch.utils.data.DataLoader(
+                self.test_dataset,
+                batch_size=self.batch_size,
+                shuffle=False,
+                num_workers=8
+            )
 
 class ChirpsCmip6DataModule(pl.LightningDataModule):
 
